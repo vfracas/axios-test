e2e3dd149ad725fa44bde97a1724e6a0e70471ec
Vincent <vincent.fracas@gmail.com>
2021-08-08 23:02:15 +0200

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,68 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'axios-test',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy'
+  ],
+
+  axios: {
+    // extra config e.g
+    baseURL: process.env.API_URL,
+    proxy: true,
+    retry: {retries: 3},
+  },
+
+  proxy: {
+    '/api/': {target:'https://api.itbook.store/1.0/search/new', pathRewrite: {'^/api/': ''}}
+  },
+
+  publicRuntimeConfig: {
+    axios: {
+      browserBaseURL: process.env.BROWSER_API_URL
+    }
+  },
+
+  privateRuntimeConfig: {
+    axios: {
+      baseURL: process.env.API_URL
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },methods:{
+    async asyncData({$axios}){
+      const results = await $axios.$get('URL', {progress: false}).then((res) => {return res.data})
+      return {results}
+    }
+  }
+}
+</script>
